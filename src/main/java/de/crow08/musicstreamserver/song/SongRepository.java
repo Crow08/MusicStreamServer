7364f08fcd6764536ae2ae96042912f085845273
xHawkyx <tobiku92@gmx.de>
2021-03-16 12:55:04 +0100

    Nvm, figured it out
    - there might still be a more efficient way (search by id, when artist is known?)

--- src/main/java/de/crow08/musicstreamserver/song/SongRepository.java
+++ src/main/java/de/crow08/musicstreamserver/song/SongRepository.java
@@ -8,6 +8,6 @@ import org.springframework.data.repository.query.Param;
 
 public interface SongRepository extends CrudRepository<Song, Long> {
   List<Song> findByTitleContains(String keyword);
-  @Query(nativeQuery = true, value = "SELECT song.path, song.title, artist.name, artist.id, song.album_id, song.artist_id FROM song INNER JOIN artist ON song.artist_id=artist.id WHERE artist.name LIKE CONCAT('%',:keyword,'%')")
+  @Query(nativeQuery = true, value = "SELECT * FROM song INNER JOIN artist ON song.artist_id=artist.id WHERE artist.name LIKE CONCAT('%',:keyword,'%')")
   List<Song> findByArtist(@Param("keyword") String keyword);
 }
