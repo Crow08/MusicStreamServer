c58f9c76f7b523d97419112068498ac54161852a
Kevin Hertfelder <kevin.game@web.de>
2023-01-15 22:14:43 +0100

    Add timeout for sync process

--- src/main/java/de/crow08/musicstreamserver/sessions/SessionUserSynchronization.java
+++ src/main/java/de/crow08/musicstreamserver/sessions/SessionUserSynchronization.java
@@ -3,6 +3,8 @@ package de.crow08.musicstreamserver.sessions;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class SessionUserSynchronization {
 
@@ -24,18 +26,29 @@ public class SessionUserSynchronization {
   public synchronized void addSyncUsers(Long userId, long mediaId, long duration) {
     if (mediaId == syncTargetMediaId && duration == syncTargetDuration) {
       System.out.println("Synced user:" + userId);
-      if(syncUsers.stream().noneMatch(syncedUser -> Objects.equals(userId, syncedUser))) {
+      if (syncUsers.stream().noneMatch(syncedUser -> Objects.equals(userId, syncedUser))) {
         this.syncUsers.add(userId);
       }
     } else if (syncTargetMediaId == -1 || syncTargetDuration == -1) {
-      syncTargetMediaId = mediaId;
-      syncTargetDuration = duration;
-      syncUsers.clear();
       System.out.println("new sync for user:" + userId);
+      newSyncTarget(mediaId, duration);
+      Timer timer = new Timer();
+      timer.schedule(new TimerTask() {
+        @Override
+        public void run() {
+          completeSync();
+        }
+      }, 5000);
       this.syncUsers.add(userId);
     } else {
       System.out.println("Sync conflict: ignoring new sync to: " + mediaId + " at: " + duration);
     }
+  }
+
+  private void newSyncTarget(long mediaId, long duration) {
+    syncTargetMediaId = mediaId;
+    syncTargetDuration = duration;
+    syncUsers.clear();
   }
 
   public synchronized void completeSync() {
